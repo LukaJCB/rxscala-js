4d8e21745e276bec9e7ac11d4d61a732d8d916d4
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2017-03-01 15:03:01 +0100

    Fix creationMethod of Rx.ajax

--- src/main/scala/rxscalajs/dom/Ajax.scala
+++ src/main/scala/rxscalajs/dom/Ajax.scala
@@ -8,7 +8,8 @@ final case class Request(url: String,
                          timeout: Int = 0,
                          headers: Map[String, String] = Map.empty,
                          crossDomain: Boolean = false,
-                         responseType: String = "")
+                         responseType: String = "",
+                         method: String = "GET")
 
 final case class Response(body: String, status: Int, responseType: String)
 
@@ -21,7 +22,8 @@ object Ajax {
       timeout = request.timeout,
       headers = request.headers.toJSDictionary,
       crossDomain = request.crossDomain,
-      responseType = request.responseType
+      responseType = request.responseType,
+      method = request.method
     ).asInstanceOf[AjaxRequest]
   }
 
